ec4fd5b6e568e07fb482bf9ac3132048f5d790ff
Bharat <bharatbhusanmantry@gmail.com>
2020-12-15 10:54:26 +0530

    testng4

---
+++ src/test/java/dockerdemo/TestNG_4.java
@@ -0,0 +1,32 @@
+package dockerdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestNG_4 {
+
+	@Test
+	public void TestNG1_test1()
+	{
+		System.out.println("TestNG1.test1");
+		WebDriver  driver= new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+	
+	@Test(groups="sequential-test")
+	public void TestNG1_test2()
+	{
+		System.out.println("TestNG1.test2");
+		WebDriver  driver= new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+	}
+	
+	@Test
+	public void TestNG1_test3()
+	{
+		System.out.println("TestNG1.test3");
+		WebDriver  driver= new FirefoxDriver();
+		driver.get("https://www.cricinfo.com");
+	}
+}
